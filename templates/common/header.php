b92dc3708c31d86263a645044164b160a58d928a
Bruno Miguel <up201504781@gcloud.fe.up.pt>
2017-11-30 18:23:44 +0000

    Completed Header and Footer css. Stuff is kinda modulated

--- templates/common/header.php
+++ templates/common/header.php
@@ -5,13 +5,17 @@
         <meta charset="UTF-8" />
         <meta name="description" content="HELPO is a tool to help you organize your every day life!"/>
         <link rel="stylesheet" type="text/css" href="css/stylesheet.css"/>
+        <link rel="stylesheet" href="css/layout.css"/>
         <noscript>Your web browser does not support JavaScript. Some features may be disabled. Updated the latest
             version of your browser.</noscript>
     </head>
 
     <body>
         <header>
-            <h1><a href="#">Helpo</a></h1>
+            <a href="index.php">
+                <img src="images/todolist.png">
+            </a>
+            <h1><a href="index.php">Helpo</a></h1>
             <h3>Your best friend in making your every day life easier</h3>
         </header>
 
