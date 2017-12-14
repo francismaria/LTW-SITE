dac2834f82db04c2b194bcbc9b05919b0e13e6bc
Francisco Maria <franciscomaria97@hotmail.com>
2017-12-14 23:45:39 +0000

    Updated add_user_task

--- templates/actions/action_add_user_to_task.php
+++ templates/actions/action_add_user_to_task.php
@@ -1,5 +1,5 @@
 <?php
-/* 
+/*
 * Copyright (C) LTW @ FEUP project authors. All rights reserved.
 *
 * Authors:
@@ -7,7 +7,7 @@
 * - Francisco <@fe.up.pt>
 * - Pedro Azevedo   <up201306026@fe.up.pt>
 */
- 
+
 require_once('../../database/db.php');
 
 session_start();
@@ -15,12 +15,14 @@ if ($_SESSION['loggedin']) {
   $db = new Database('../../database/helpo.db');
   $user = $db->get_user_by_name(htmlspecialchars($_POST['user_name']));
   $user_id = $db->get_user_by_project_task_id($_POST['project_id'], $task['task_id']);
-  
+
   if($user_id['user_id'] != '' && $user_id['user_id'] != NULL && $user_id['user_id'] != 0){
-	$db->update_user_to_task(htmlspecialchars($_POST['project_id']),htmlspecialchars($_POST['task_id']),$user['USER_ID']);
+	   $db->update_user_to_task(htmlspecialchars($_POST['project_id']),htmlspecialchars($_POST['task_id']),$user['USER_ID']);
+  }else{
+     $db->add_user_to_task(htmlspecialchars($_POST['project_id']),htmlspecialchars($_POST['task_id']),$user['USER_ID']);
   }
-  $db->add_user_to_task(htmlspecialchars($_POST['project_id']),htmlspecialchars($_POST['task_id']),$user['USER_ID']);
-  
-  
+
+
+
   echo $user['USER_ID'];
 }
